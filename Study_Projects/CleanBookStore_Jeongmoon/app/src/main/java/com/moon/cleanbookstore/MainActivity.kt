d99ab0c4284d672c6c87d4650f1b1f715ecdca7b
ExpeditionMoon <dnjswon1025@gmail.com>
2026-03-31 15:59:41 +0900

    chore: [CleanBookStore_Jeongmoon] 프로젝트 초기 생성

---
+++ Study_Projects/CleanBookStore_Jeongmoon/app/src/main/java/com/moon/cleanbookstore/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.moon.cleanbookstore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.moon.cleanbookstore.ui.theme.CleanBookStore_JeongmoonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CleanBookStore_JeongmoonTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CleanBookStore_JeongmoonTheme {
+        Greeting("Android")
+    }
+}
